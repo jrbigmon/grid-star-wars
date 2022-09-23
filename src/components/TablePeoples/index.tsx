9c139b11f411a2da7613f5bae237919f4e3feb4c
jrbigmon <junioterriaga0@gmail.com>
2022-09-23 17:08:20 -0300

    Criado função para buscar os valores da api e trazer para criar a tabela

--- src/components/TablePeoples/index.tsx
+++ src/components/TablePeoples/index.tsx
@@ -1,16 +1,78 @@
-import TableDataPeople from "./TableDataPeople";
-import TableHeaderPeople from "./TableHeaderPeople";
+import TableDataPeople from "./TableDataPeople"
+import TableHeaderPeople from "./TableHeaderPeople"
+import starWarsApi from "../../services/starwarsApi"
+import { useState, useEffect } from "react"
+import Button from "../Button"
+
+interface DataPeople {
+  name: string
+    height: string
+    mass: string 
+    hair_color: string
+    skin_color: string
+    eye_color: string
+    birth_year: string
+    gender: string
+    homeworld: string
+    films: []
+    species: []
+    vehicles: []
+    starships: []
+}
 
 function TablePeoples() {
+
+  async function getPeoples() {
+    const response = await starWarsApi.get(`people/${page}`)
+    return setPeoples(response.data.results)
+  }
+
+  function setNumberPage(){
+
+  }
+
+  const [peoples, setPeoples] = useState<DataPeople[]>([])
+  const [page, setPage] = useState<string>('')
+
+  useEffect(() => {
+    getPeoples()
+  }, [])
+
+
   return (
-    <table className="table-fixed">
-      <thead>
-        <TableHeaderPeople />
-      </thead>
-      <tbody>
-        <TableDataPeople />
-      </tbody>
-    </table>
+    <div className="flex justify-center flex-col">
+      <table className="table-fixed">
+        <thead>
+          <TableHeaderPeople />
+        </thead>
+        <tbody>
+          {peoples.map((people, i) => {
+            return (
+              <TableDataPeople
+                key={i}
+                name={people.name}
+                height={people.height}
+                mass={people.mass}
+                hairColor={people.hair_color}
+                skinColor={people.skin_color}
+                eyeColor={people.eye_color}
+                birthYear={people.birth_year}
+                gender={people.gender}
+                homeworld={people.homeworld}
+                films={people.films}
+                species={people.species}
+                vehicles={people.vehicles}
+                starships={people.starships}
+              />
+            )
+          })}
+        </tbody>
+      </table>
+      <div className="flex justify-center">
+        <Button text={'Previus'} />
+        <Button text={'Next'} />
+      </div>
+    </div>
   );
 }
 
