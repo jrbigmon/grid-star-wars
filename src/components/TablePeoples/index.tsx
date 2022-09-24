21a570a19547dd55e4c0f0a7df9f14b9544fd64f
jrbigmon <junioterriaga0@gmail.com>
2022-09-23 21:19:30 -0300

    Adicionado setEyeColos para criar uma lista das cores

--- src/components/TablePeoples/index.tsx
+++ src/components/TablePeoples/index.tsx
@@ -3,21 +3,22 @@ import TableHeaderPeople from "./TableHeaderPeople"
 import starWarsApi from "../../services/starwarsApi"
 import { useState, useEffect } from "react"
 import Button from "../Button"
+import SearchBar from "../SearchBar"
 
 interface DataPeople {
   name: string
-    height: string
-    mass: string 
-    hair_color: string
-    skin_color: string
-    eye_color: string
-    birth_year: string
-    gender: string
-    homeworld: string
-    films: []
-    species: []
-    vehicles: []
-    starships: []
+  height: string
+  mass: string 
+  hair_color: string
+  skin_color: string
+  eye_color: string
+  birth_year: string
+  gender: string
+  homeworld: string
+  films: []
+  species: []
+  vehicles: []
+  starships: []
 }
 
 function TablePeoples() {
@@ -25,21 +26,36 @@ function TablePeoples() {
   const [page, setPage] = useState<string>('people')
   const [next, setNext] = useState<string>('')
   const [previous, setPrevious] = useState<string>('')
+  const [eyeColors, setEyeColors] = useState<[]>([])
 
   async function getPeoples() {
     const response = await starWarsApi.get(`${page}`)
-    setNext(response.data.next)
-    setPrevious(response.data.previous)
     setPeoples(response.data.results)
   }
+  async function getNextAndPrevius() {
+    const response = await starWarsApi.get(`${page}`)
+    setNext(response.data.next)
+    setPrevious(response.data.previous)
+  }
+  async function getEyeColors() {
+    const response = await starWarsApi.get(`${page}`)
+    const eyeColors = response.data.results.map((people: DataPeople) => {
+      return people.eye_color
+    })
+    setEyeColors(eyeColors)
+  }
+
 
   useEffect(() => {
     getPeoples()
+    getNextAndPrevius()
+    getEyeColors()
   }, [page])
 
 
   return (
     <div className="flex justify-center flex-col">
+      <SearchBar eyeColors={eyeColors}/>
       <table className="table-fixed">
         <thead>
           <TableHeaderPeople />
