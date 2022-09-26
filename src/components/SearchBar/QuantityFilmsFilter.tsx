c39b900add1b824449a72eaf072dd96c698da8f8
jrbigmon <junioterriaga0@gmail.com>
2022-09-25 22:34:38 -0300

    Modificando dimensão do input

--- src/components/SearchBar/QuantityFilmsFilter.tsx
+++ src/components/SearchBar/QuantityFilmsFilter.tsx
@@ -7,7 +7,7 @@ function QuantityFilmsFilter(props: DataPeople){
         <div className="mb-5 flex flex-col justify-center w-max mx-4">
             <label htmlFor="quantityFilms" className="block text-[#FFDF00] font-black">Maior que:</label>
             <input
-                className="bg-slate-500 text-white w-22 rounded-sm overflow-hidden"
+                className="bg-slate-500 text-white w-[100px] rounded-sm"
                 id="quantityFilms"
                 type='number'
                 onChange={e => props.methodFilms(e.target.value)}
